0191a049ea550b00af29f3366c42ce2d9411f5c4
leonardofrp5 <leonardofrp5@gmail.com>
2020-08-12 19:57:28 -0500

    I created some views like home, error, headers, characters

---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  const view = `
+    <div class="Header-main">
+      <div class="header-logo">
+        <h1>
+          <a href="/">
+            Rick and Morty
+          </a>
+        </h1>
+      </div>
+      <div>
+        <a href="#/about/">
+          About
+        </a>
+      </div>
+    </div>
+  `
+  return view;
+}
+
+export default Header;
